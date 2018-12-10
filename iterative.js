b7335e26ed0beff4fdccfca210be84db68c7950b
Reldalk <Reldalk@gmail.com>
2018-12-10 17:34:45 -0500

    done to fib

--- iterative.js
+++ iterative.js
@@ -58,3 +58,50 @@ function stringSplitter(sentence, separator) {
 
 console.log(stringSplitter('hello world', ' '));
 console.log(stringSplitter('hellolll', 'l'));
+
+function binaryIterative(number){
+  let binary = '';
+  while(number > 0){
+    binary = Math.floor(number % 2) + binary;
+    number = Math.floor(number / 2);
+  }
+  return binary;
+}
+
+console.log(binaryIterative(25));
+
+function factorialIterative(number){
+  let factorial = 1;
+  for(let i = number; i > 0; i--){
+    factorial *= i;
+  }
+  return factorial;
+}
+
+console.log(factorialIterative(5));
+/*
+function fibIterative(number){
+  if(number === 0){
+    return 0;
+  }
+  let sums = [0, 1];
+  for(let i = 1; i < number; i+=1){
+    sums.push(sums[i-1] + sums[i]);
+  }
+  return sums[sums.length - 1];
+}
+*/
+
+function fibIterative(number){
+  if(number <= 1){
+    return number;
+  }
+  let sums = [1, 1];
+  let j = 0;
+  for(let i = 2; i < number; i++){
+    j+=1;
+    sums[j%2] = sums[0] + sums[1];
+  }
+  return sums[j%2];
+}
+console.log(fibIterative(2));
