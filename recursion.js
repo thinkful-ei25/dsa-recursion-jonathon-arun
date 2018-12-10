7b70a55ccb251034efdea1e0f7fefc4c6aa74878
Reldalk <Reldalk@gmail.com>
2018-12-10 11:53:33 -0500

    reverseStirng and triangularNumber complete

--- recursion.js
+++ recursion.js
@@ -32,3 +32,36 @@ let arrayDouble = function(array) {
 console.log(arrayDouble([2, 6, 10]));
 console.log(arrayDouble([1, 2, 3]));
 console.log(arrayDouble([2, 5, 6]));
+
+
+// reverseString
+
+// Input: a string
+// Output: same string in reverse order
+//Input to recursive call: string - char[1...n]
+//Output to recursive call: char[1...n] reversed
+
+function reverseString(string){
+  if(string === ''){
+    return '';
+  }
+  return reverseString(string.slice(1)) + string[0];
+}
+
+console.log(reverseString('a quick brown fox jumped over the lazy frog'));
+
+// triangularNumber
+
+// Input: positive Integer
+// Ouput: An output of the number of dots in the triangle
+//Input to recursive call: (n-1)
+//Output to recursive call: triangular number for that row
+
+function triangularNumber(n){
+  if(n <= 1){
+    return 1;
+  }
+  return n + triangularNumber(n-1);
+}
+
+console.log([1, 2, 3, 4, 5, 6, 7, 8, 9].map(triangularNumber));
