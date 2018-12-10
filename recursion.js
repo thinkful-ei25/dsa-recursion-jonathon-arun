66a924f37dce3f25a9f453b74ac6cadacb3f9901
Arun Seehra <seehra.a@gmail.com>
2018-12-10 10:41:12 -0600

    Array double exercies

--- recursion.js
+++ recursion.js
@@ -12,3 +12,23 @@ let countSheep = function(numberOfSheep) {
 };
 
 countSheep(3);
+
+// Array double
+
+// Input: array of unknown length of numbers
+// Output: array of unknown lenght of numbers with values doubled
+// Input to recursive call: array.slice(1)
+// Output to recurisive call: array[0] + arrayDouble(...)
+
+let arrayDouble = function(array) {
+  if (array.length === 0) {
+    return [];
+  }
+
+  const doubled = [array[0] * 2];
+  return doubled.concat(arrayDouble(array.slice(1)));
+};
+
+console.log(arrayDouble([2, 6, 10]));
+console.log(arrayDouble([1, 2, 3]));
+console.log(arrayDouble([2, 5, 6]));
